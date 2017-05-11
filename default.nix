844ffd78099802975bd639315e4e0a8453d10314
Frederik Rietdijk <fridh@fridh.nl>
2017-05-11 15:33:35 +0200

    Add nix expression

---
+++ default.nix
@@ -0,0 +1,21 @@
+{ buildPythonPackage
+, pytest
+, cython
+, cytoolz
+, multipledispatch
+, numpy
+, noisy
+}
+
+buildPythonPackage rec {
+  name = "streaming-${version}";
+  version = "dev";
+
+  src = ./.;
+
+  checkInpus = [ pytest ];
+  buildInputs = [ cython ];
+  propagatedBuildInputs = [ cytoolz multipledispatch numpy noisy ];
+
+  doCheck = false;
+}
